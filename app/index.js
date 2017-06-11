62fc81692476fcd3a48bbdb08ee3e271d351b413
Peter Laraia <pvlaraia@gmail.com>
2017-06-11 05:55:29 +0000

    basic component rendering and simple state management

---
+++ app/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { App } from './components/app';
+
+require('./index.css');
+
+
+render(<App />, document.getElementById('app'));
